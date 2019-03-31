e0633a6a18684fd56db492c18294833d00d9db19
death-kun <deathikun@gmail.com>
2019-03-31 17:53:15 +0700

    Первый коммит

--- fixture/Application.py
+++ fixture/Application.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 
 
+
 class Application:
 
     def __init__(self):
         self.driver = webdriver.Chrome('D:\python test\camdrive\chromedriver.exe')
+        driver = self.driver
+        driver.delete_all_cookies()
+
+    def Authorization(self):
+        driver = self.driver
+        driver.get('http://test.camdrive.org/')
+        login = driver.find_element_by_xpath('//*[@id="login-box"]/form/table/tbody/tr[1]/td[2]/input')
+        login.send_keys('autotest')
+        password = driver.find_element_by_xpath('//*[@id="login-box"]/form/table/tbody/tr[2]/td[2]/div/input[1]')
+        password.send_keys('autotest')
 
 
 
 
-
-
+    def Stop(self):
+        self.driver.quit()
---
+++ test/camdrive_autotest.py
@@ -0,0 +1,3 @@
+
+def test_log_out(app):
+    app.Authorization()
