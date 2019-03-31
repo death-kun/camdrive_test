6d2cc94989893f52c017dece7898f124f1b7ca43
death-kun <deathikun@gmail.com>
2019-03-31 15:50:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# camdrive
---
+++ fixture/Application.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+
+class Application:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome('D:\python test\camdrive\chromedriver.exe')
+
+
+
+
+
+
