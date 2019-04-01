8adaf3abe6ec238954aae83bbae1de85e937d3ff
Mikhail <deathikun@gmail.com>
2019-04-01 14:59:55 +0700

    Изменение xpath для логина и пароля

--- fixture/Application.py
+++ fixture/Application.py
@@ -5,17 +5,17 @@ from selenium import webdriver
 class Application:
 
     def __init__(self):
-        self.driver = webdriver.Chrome('D:\python test\camdrive\chromedriver.exe')
+        self.driver = webdriver.Chrome('D:\python test\camdrive_test\chromedriver.exe')
         driver = self.driver
         driver.delete_all_cookies()
 
     def Authorization(self):
         driver = self.driver
         driver.get('http://test.camdrive.org/')
-        login = driver.find_element_by_xpath('//*[@id="login-box"]/form/table/tbody/tr[1]/td[2]/input')
+        login = driver.find_element_by_xpath('//input[@name="username"]')
         login.send_keys('autotest')
-        password = driver.find_element_by_xpath('//*[@id="login-box"]/form/table/tbody/tr[2]/td[2]/div/input[1]')
-        password.send_keys('autotest')
+        password = driver.find_element_by_xpath('//input[@name="password"]')
+        password.send_keys('111')
 
 
 
