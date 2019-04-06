2354c47b2727ed386ffb1eef6bf507e02ed6f887
death-kun <deathikun@gmail.com>
2019-04-06 13:06:28 +0700

    Проверки через if

--- fixture/Application.py
+++ fixture/Application.py
@@ -21,24 +21,23 @@ class Application:
         driver.find_element_by_xpath('//input[@name="password"]').send_keys('111')
         driver.find_element_by_id('login').click()
         time.sleep(1)
-        warning = driver.find_element_by_xpath('//div[@id="login-box"]/div[2]/span/p')
-        # Проверка того, что указан не вернный пароль
-        if not warning:
-            raise AssertionError('Введены корректные значения')
+        #Проверка того, что указан не вернный пароль
+        if driver.find_element_by_xpath('//div[@id="login-box"]/div[2]/span/p').text == "Ошибка идентификации. Проверьте правильность логина и введите Ваш пароль еще раз. Количество попыток ограничено!.":
+            print('Проверка того, что указан не вернный пароль. Проверка прошла успешно. Введен некорректный пароль')
+        else:
+            print('Проверка того, что указан не вернный пароль. Проверка провалилась. Введены корректные значения')
 
     def password_visibility_check(self):
         driver = self.driver
         self.open_home_page()
-        entry_field = driver.find_element_by_xpath('//input[@name="password"]').send_keys('111')
+        driver.find_element_by_xpath('//input[@name="password"]').send_keys('111')
         time.sleep(1)
-        eye = driver.find_element_by_xpath('//div[@id="login-box"]/form/table/tbody/tr[2]/td[2]/div/div').click()
-
-        # if entry_field.get_attribute('111'):
-        #     print('Указан пароль 111')
-        # else:
-        #     print('Пароль скрыт')
-
-
+        driver.find_element_by_xpath('//div[@id="login-box"]/form/table/tbody/tr[2]/td[2]/div/div').click()
+        #Проверка, что при нажатии на "глаз" видно введенный пароль
+        if driver.find_element_by_xpath('//input[@name="password"]').get_attribute('value') == "111":
+            print('Проверка, что при нажатии на "глаз" видно введенный пароль. ППроверка прошла успешно. Пароль - 111')
+        else:
+            print('Проверка, что при нажатии на "глаз" видно введенный пароль. Проверка провалилась. Пароль не видно')
 
     def login_with_valid_data(self):
         driver = self.driver
@@ -47,9 +46,11 @@ class Application:
         driver.find_element_by_xpath('//input[@name="password"]').send_keys('autotest')
         driver.find_element_by_id('login').click()
         time.sleep(1)
-        # Проверка того, что указан вернный пароль
-        if driver.find_element_by_xpath('//div[@id="title"]/div').is_displayed():
-            print('Пароль введен корректно')
+        #Проверка того, что указан вернный логин и пароль
+        if driver.find_element_by_xpath('//div[@id="title"]/div').get_attribute('value') == "Онлайн":
+            print('Проверка того, что указан вернный логин и пароль. Проверка прошла успешно. Логин и пароль введены корректно')
+        else:
+            print('Проверка того, что указан вернный логин и пароль. Данные введены некорректно')
 
 
     def Stop(self):
