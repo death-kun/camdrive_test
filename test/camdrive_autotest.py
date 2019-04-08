5e7eaa503364787a3c827222656b1d63918834df
Mikhail <deathikun@gmail.com>
2019-04-08 11:06:50 +0700

    орфография

--- test/camdrive_autotest.py
+++ test/camdrive_autotest.py
@@ -1,8 +1,8 @@
 
 def test_log_out(app):
-    app.Authorization.login_with_incorrect_data() #Проверка того, что указан не вернный пароль
+    app.Authorization.login_with_incorrect_data() #Проверка того, что указан не верный пароль
     app.Authorization.password_visibility_check() #Проверка, что при нажатии на "глаз" видно введенный пароль
-    app.Authorization.login_with_valid_data() #Проверка того, что указан вернный логин и пароль
+    app.Authorization.login_with_valid_data() #Проверка того, что указан верный логин и пароль
 
 def test_forgot_your_password(app):
     app.forgot_your_password() #Проверка перехода на форму "Забыли пароль?"
