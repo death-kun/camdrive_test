4d6dc85404b3fdd60fd814be15d659d81be069fe
Mikhail <deathikun@gmail.com>
2019-06-14 10:14:00 +0700

    Рефакторинг

--- fixture/monitoring/Monitoring.py
+++ fixture/monitoring/Monitoring.py
@@ -117,38 +117,47 @@ class monitoring:
     def click_CD100_E75A_ms3_dev(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_12597"]/a').click()  # камера CD100(E75A)_ms3_dev
+        self.camera_name = self.title()
 
     def click_CD100_E778_ms5(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_6827"]/a').click()  # камера CD100(E778)_ms5_ПЗ
+        self.camera_name = self.title()
 
     def click_CD600_EF78_ms6_serv(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_12601"]/a').click()  # камера CD600(EF78)_ms6_serv
+        self.camera_name = self.title()
 
     def click_N1001_3A00_bwd(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_14875"]/a').click()  # камера N1001(3A00)_bwd
+        self.camera_name = self.title()
 
     def click_CD100_E772_ms4(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_6830"]/a').click()  # камера CD100(E772)_ms4_ПЗ
+        self.camera_name = self.title()
 
     def click_CD310_2E51_ms4_dev(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_13959"]/a').click()  # камера CD310(2E51)_ms4_dev
+        self.camera_name = self.title()
 
     def click_CD320_AA78_ms5(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_11460"]/a').click()  # камера CD320(AA78)_ms5_Пр_С
+        self.camera_name = self.title()
 
     def click_CD320_AA06_ms3_dev(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_12603"]/a').click()  # камера CD320(AA06)_ms3_dev
+        self.camera_name = self.title()
 
     def click_CD630_910D_ms6_dev(self):
         driver = self.app.driver
         driver.find_element_by_xpath('//*[@id="node_12602"]/a').click()  # камера CD630(910D)_ms6_dev
+        self.camera_name = self.title()
 
 
     #Камеры для проверки на тестовом сервере
