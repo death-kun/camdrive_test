92a2603b6446f4f1ff05bda2759f94844704c30e
death-kun <deathikun@gmail.com>
2019-07-09 19:35:59 +0700

    Добавлен запрос на получение плеера.

--- fixture/monitoring/Online.py
+++ fixture/monitoring/Online.py
@@ -15,18 +15,17 @@ class checkonlune:
         data = {'username': 'autotest', 'password': 'autotest'}
         d = {'action': 'screen_play', 'data': '{"num":1,"id":"cd2aec6529925a5632118fb0974a4a97"}'}
         url1 = 'https://test.camdrive.org/'
-        url2 = 'https://test.camdrive.org/camdrive/assets/flash/BewardVideoPlayerOnline_1.51.swf' #плеер
+        self.url2 = 'https://test.camdrive.org/camdrive/assets/flash/BewardVideoPlayerOnline_1.51.swf' #плеер
         url3 = 'https://test.camdrive.org/online/get_screens_items' #получить информацию по видео
         error_code = 'outOfRangeMaxConnectionsLimitTariff' #код серверной ошибки
         error_code_1 = 'deviceNotConnectedToServer'  #код ошибки что камера не подключена
-        z = requests.Session()
 
         s = requests.Session()  # Создание сеанс
         a = s.post(url1, data=data)  # Авторизация запросом
-        c = a.cookies.get_dict()  # Получаем куки
-        print(c)
+        self.c = a.cookies.get_dict()  # Получаем куки
+        print(self.c)
 
-        request_video = s.post(url=url3, data=d, cookies=c, headers={'Connection':'close'}) # Отправка запроса на получение видео
+        request_video = s.post(url=url3, data=d, cookies=self.c, headers={'Connection':'close'}) # Отправка запроса на получение видео
         request_video2 = request_video.text
         s.close()
         print(request_video2)
@@ -38,4 +37,19 @@ class checkonlune:
     def online_gui(self):
         driver = self.app.driver
         self.app.Player.add_camera_player1()
+
+    def request_player(self):
+
+        s = requests.Session()
+        r = s.get(url=self.url2, cookies=self.c, headers={"Accept": "*/*",
+                                                        "Accept-Encoding": "gzip, deflate, br",
+                                                        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                                                        "Connection": "keep-alive",
+                                                        "Referer": "https://test.camdrive.org/online",
+                                                        "Host": "test.camdrive.org",
+                                                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+                                                        "X-Requested-With": "ShockwaveFlash/32.0.0.207"})
+        r2 = r.headers.items()
+        s.close()
+        print(r2)
 
--- model/monitoring_online/CD120_D521.py
+++ model/monitoring_online/CD120_D521.py
@@ -6,10 +6,11 @@ class CD120_D521_online:
         self.app = app
 
     def detection_of_online(self):
-        self.app.open_home_page()
-        self.app.login_autotest()
-        self.app.Camera_List.click_camera_CD120_D521()
-        self.app.Online.online_gui()
-        time.sleep(10)
+        # self.app.open_home_page()
+        # self.app.login_autotest()
+        # self.app.Camera_List.click_camera_CD120_D521()
+        # self.app.Online.online_gui()
+        # time.sleep(10)
         self.app.Online.request_online()
+        self.app.Online.request_player()
 
