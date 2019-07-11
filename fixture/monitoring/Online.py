45fbcd74ba9628aaac87d0527d9f3902c839e372
Mikhail <deathikun@gmail.com>
2019-07-11 17:52:24 +0700

    Изменены переменные для скриншотов.

--- fixture/monitoring/Online.py
+++ fixture/monitoring/Online.py
@@ -18,26 +18,26 @@ class checkonlune:
         try:
             WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="flash_1"]')))
             i = 0
-            p1 = 0
+            old_screenshot_player = 0
             while i < 300: # 5 минут в течение, которых проверяется стабильность онлайна
                 driver.find_element_by_xpath('//*[@id="screens"]/div[1]/div/div[2]/div').screenshot("1.png") #делаем скриншот видеоплеера
                 screenshot_error = Image.open("D:/python test/camdrive_test/test/error_code_0.png") #скриншот с ошибкой
                 screenshot_progress_bar = Image.open("D:/python test/camdrive_test/test/progress_bar.png") #скриншот с прогрессбаром
-                self.p = Image.open("1.png")
+                self.screenshot_player = Image.open("1.png")
 
                 # Среднеквадратическая разница. Чтобы измерить, насколько похожи два изображения, вы можете рассчитать среднеквадратичное (RMS) значение разницы между изображениями.
                 # Если изображения абсолютно идентичны, это значение равно нулю.
                 # Следующая функция использует функцию разности , а затем вычисляет среднеквадратичное значение по гистограмме полученного изображения
-                h1 = self.p.histogram()
+                h1 = self.screenshot_player.histogram()
                 h2 = screenshot_error.histogram()
                 rms = math.sqrt(reduce(operator.add, map(lambda a, b: (a - b) ** 2, h1, h2)) / len(h1)) #определяем разницу между скриншотом плеера и скриншотом серверной ошибки
-                # print(rms, ' сравнение с ошибкой')
+                print(rms, ' сравнение с ошибкой')
 
                 h3 = screenshot_progress_bar.histogram()
                 rms2 = math.sqrt(reduce(operator.add, map(lambda a, b: (a - b) ** 2, h1, h3)) / len(h1)) #определяем разницу между скриншотом плеера и скриншотом прогрессбара
-                # print(rms2, ' сравнение с прогрессбаром')
+                print(rms2, ' сравнение с прогрессбаром')
 
-                if self.p == p1:
+                if self.screenshot_player == old_screenshot_player:
                     print('Не идет онлайн')
                 elif rms < 15.0:
                     print('Отображается серверное сообщение "Превышено максимальное допустимое количество одновременных подключений".')
@@ -46,8 +46,8 @@ class checkonlune:
                 else:
                     print('Идет онлайн')
                 time.sleep(1)
-                p1 = self.p
+                old_screenshot_player = self.screenshot_player
                 i += 1
 
         except TimeoutException:
-            print('Не загрузился плеер')
+            print('Не загрузилось видео')
