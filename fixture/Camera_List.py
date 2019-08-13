17845064156f672a8a27bd2fef1c1f1408afe11b
Mikhail <deathikun@gmail.com>
2019-08-13 17:32:38 +0700

    Добавлены две ТУДУшки.

--- fixture/Camera_List.py
+++ fixture/Camera_List.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+
+#TODO :: Рефакторинг выбора рабочих камер.
+#TODO :: Добавить проверки на раскрыты группы или нет.
 
 class cameralist:
 
