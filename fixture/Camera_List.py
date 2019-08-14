3a0d0f3eef7ed101e23afa2805a2ef4aeac55254
Mikhail <deathikun@gmail.com>
2019-08-14 09:34:12 +0700

    Добавлена проверка на открыто дерево или нет.

--- fixture/Camera_List.py
+++ fixture/Camera_List.py
@@ -23,54 +23,90 @@ class cameralist:
 
     def click_camera_CD100_E75A_ms3_dev(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.second_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12605"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_12597']/a")))
         self.click_CD100_E75A_ms3_dev = driver.find_element_by_xpath('//*[@id="node_12597"]/a').click()  # камера CD100(E75A)_ms3_dev
         self.app.Monitoring.camera_title()
 
     def click_camera_CD100_E772_ms4(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.second_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12605"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_6830']/a")))
         self.click_CD100_E772_ms4 = driver.find_element_by_xpath('//*[@id="node_6830"]/a').click()  # камера CD100(E772)_ms4_ПЗ
         self.app.Monitoring.camera_title()
 
     def click_camera_CD100_E778_ms5(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.second_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12605"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_6827']/a")))
         self.click_CD100_E778_ms5 = driver.find_element_by_xpath('//*[@id="node_6827"]/a').click() # камера CD100(E778)_ms5_ПЗ
         self.app.Monitoring.camera_title()
 
     def click_camera_CD310_2E51_ms4_dev(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.first_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12604"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_13959']/a")))
         self.click_CD310_2E51_ms4_dev = driver.find_element_by_xpath('//*[@id="node_13959"]/a').click()  # камера CD310(2E51)_ms4_dev
         self.app.Monitoring.camera_title()
 
     def click_CD320_AA06_ms3_dev(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.first_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12604"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_12603']/a")))
         self.click_CD320_AA06_ms3_dev = driver.find_element_by_xpath('//*[@id="node_12603"]/a').click()  # камера CD320(AA06)_ms3_dev
         self.app.Monitoring.camera_title()
 
     def click_camera_CD320_AA78_ms5(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.first_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12604"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_11460']/a")))
         self.click_CD320_AA78_ms5 = driver.find_element_by_xpath('//*[@id="node_11460"]/a').click()  # камера CD320(AA78)_ms5_Пр_С
         self.app.Monitoring.camera_title()
 
     def click_CD600_EF78_ms6_serv(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.first_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12604"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_12601']/a")))
-        self.click_CD600_EF78_ms6_serv = driver.find_element_by_xpath('//*[@id="node_12601"]/a'.click())  # камера CD600(EF78)_ms6_serv
+        self.click_CD600_EF78_ms6_serv = driver.find_element_by_xpath('//*[@id="node_12601"]/a').click()  # камера CD600(EF78)_ms6_serv
         self.app.Monitoring.camera_title()
 
     def click_camera_CD630_910D_ms6_dev(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.first_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12604"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_12602']/a")))
         self.click_CD630_910D_ms6_dev = driver.find_element_by_xpath('//*[@id="node_12602"]/a').click()  # камера CD630(910D)_ms6_dev
         self.app.Monitoring.camera_title()
 
     def click_camera_N1001_3A00_bwd(self):
         driver = self.app.driver
+        try:
+            self.app.Tree.second_tree()
+        except:
+            driver.find_element_by_xpath('//*[@id="node_12605"]/ins').click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='node_14875']/a")))
         self.click_N1001_3A00_bwd = driver.find_element_by_xpath('//*[@id="node_14875"]/a').click()  # камера N1001(3A00)_bwd
         self.app.Monitoring.camera_title()
