71c88a32c7c333190ec291b758557ebf7ccf7c7e
Mikhail <deathikun@gmail.com>
2019-08-22 09:14:27 +0700

    Исправлен метод авторизации.

--- model/monitoring_archive/camera_N1001_3A00_bwd.py
+++ model/monitoring_archive/camera_N1001_3A00_bwd.py
@@ -7,7 +7,7 @@ class N1001_3A00_bwd:
 
     def detection_of_archive(self):
         self.app.Monitoring.site_opening()
-        self.app.Monitoring.login_autotest()
+        self.app.Monitoring.login_monitoring()
         self.app.Camera_List.click_camera_N1001_3A00_bwd()
         self.app.Monitoring.delete_txt()
         self.app.Monitoring.open_schedule_open_archive()
