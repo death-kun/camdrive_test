1e9a9b951157a85a6afcc73461c4fc6506965bed
Mikhail <deathikun@gmail.com>
2019-10-16 10:38:45 +0700

    Убраны лишние закомментированные строки.

--- conftest.py
+++ conftest.py
@@ -1,23 +1,7 @@
 import pytest
-import json
 from fixture.Application import Application
-import os.path
-
 
 fixture = None
-# target = None
-#
-# @pytest.fixture()
-# def app(request):
-#     global fixture
-#     global target
-#     if target is None:
-#         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target", default="target.json"))
-#         with open(config_file) as f:
-#             target = json.load(f)
-#     if fixture is None or not fixture.is_valid():
-#         fixture = Application()
-#     return fixture
 
 @pytest.fixture()
 def app(request):
@@ -29,13 +13,9 @@ def app(request):
             fixture = Application()
     return fixture
 
-
 @pytest.fixture(scope='session', autouse=True)
 def stop(request):
     def fin():
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
-
-# def oytest_addoption(parser):
-#     parser.addoption("--target", action="store", defaut="target.json")
